5137bbeca0111adc911047844b04b8b611c9d37d
jonaszeutzius <jonas.zeut@gmail.com>
2023-09-05 16:13:11 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Visualization } from './visualization';
+
+function App() {
+  return (
+    <div className="App">
+      {Visualization()}
+    </div>
+  );
+}
+
+export default App;
